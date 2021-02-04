38d4c2d383d2a8eeddbbf3b6c7573c1a547c46a1
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-02-03 20:38:27 -0500

    was missing an equal sign in row count

--- models/ceoModel.js
+++ models/ceoModel.js
@@ -3,7 +3,8 @@
 const db = require('./conn');
 
 class CEOModel {
-    constructor(name, slug, year) {
+    constructor(id, name, slug, year) {
+        this.id = id;
         this.name = name;
         this.slug = slug;
         this.year = year;
@@ -16,6 +17,15 @@ class CEOModel {
 
     static async getBySlug(slug) {
         const response = await db.one(`SELECT * FROM apple_ceos WHERE slug = '${slug}';`);
+        return response;
+    }
+    static async addEntry (name, slug, year) {
+        const response = await db.result(`INSERT INTO apple_ceos (name, slug, year) VALUES ($1, $2, $3)`, [name, slug, year]);
+        return response;
+    }
+
+    async deleteEntry() {
+        const response = await db.result(`DELETE FROM apple_ceos WHERE id = $1`, [this.id]);
         return response;
     }
 }
--- routes/ceos.js
+++ routes/ceos.js
@@ -1,6 +1,7 @@
 const express = require('express'),
     router = express.Router(),
-    ceoModel = require('../models/ceoModel')
+    ceoModel = require('../models/ceoModel'),
+    slugify = require('slugify');
 
 router.get('/', async (req, res) => {
     const ceosData = await ceoModel.getAll();
@@ -24,7 +25,7 @@ router.get('/:slug', async (req, res) => {
         res.render('template', {
             locals: {
                 title: `${executive.name}'s info`,
-                executive
+                executive,
             },
             partials: {
                 body: 'partials/ceo-details',
@@ -32,6 +33,33 @@ router.get('/:slug', async (req, res) => {
         });
     } else {
         res.status(404).send(`No CEO with the slug of ${slug}. Sorry 😢`)
+    }
+});
+
+router.post('/', async (req, res) => {
+    const { ceo_name, ceo_year} = req.body;
+    const slug = slugify(ceo_name, {
+        replacement: '_',
+        lower: true,
+        strict: true
+    });
+    const response = await ceoModel.addEntry(ceo_name, slug, ceo_year);
+    if (response.rowCount >= 1) {
+        res.redirect('/ceos')
+    } else{
+        res.sendStatus(500)
+    }
+});
+
+router.post('/delete', async (req, res) => {
+    const { ceo_id } = req.body;
+    // new INSTANCE of the ceoModel
+    const ceo = new ceoModel(ceo_id);
+    const response = await ceo.deleteEntry();
+    if (response.rowCount >= 1) {
+        res.redirect('/ceos')
+    } else{
+        res.sendStatus(500)
     }
 });
 
